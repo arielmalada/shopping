f6c72e7f5f50a927d744a79bc13aee2cdae80546
arielmalada <arielmalada@outlook.com>
2022-12-24 14:45:53 +0200

    feat: data section

--- src/views/DetailPage/DetailPage.page.tsx
+++ src/views/DetailPage/DetailPage.page.tsx
@@ -1,16 +1,50 @@
 //#region IMPORTS
 
+import { Typography } from "@material-tailwind/react";
 import React, { useContext } from "react";
-import { ProductContext, ProductProvider } from "../../contexts/ProductProvider";
+import {
+  ProductContext,
+  ProductProvider,
+} from "../../contexts/ProductProvider";
 //#endregion
 
 //#region SECONDARY COMPONENT
 const Pictures: React.FC = () => {
   const productValue = useContext(ProductContext);
-  return(
-    <img src={productValue?.thumbnail} alt={productValue?.title || "empty image"}/>
-  )
-}
+  return (
+    <img
+      src={productValue?.thumbnail}
+      alt={productValue?.title || "empty image"}
+    />
+  );
+};
+
+const Data: React.FC = () => {
+  const productValue = useContext(ProductContext);
+  const isPriceReduced = (productValue?.discountPercentage || 0) > 0;
+  const finalPrice =
+    isPriceReduced && productValue
+      ? productValue.price -
+        Math.round(productValue.price * productValue.discountPercentage * 0.01)
+      : productValue?.price;
+  return (
+    <div>
+      <Typography variant="h2">{productValue?.title}</Typography>
+      <div className="flex space-x-2">
+        {isPriceReduced && (
+          <div className="text-red-700">
+            <Typography variant="small">Deal</Typography>
+            <Typography>{productValue?.discountPercentage} %</Typography>
+          </div>
+        )}
+        <div>
+          <Typography>{finalPrice}</Typography>
+          {isPriceReduced && <Typography variant="small">was {productValue?.price}</Typography>}
+        </div>
+      </div>
+    </div>
+  );
+};
 //#endregion
 
 //#region MAIN COMPONENT
@@ -19,10 +53,9 @@ const DetailPage: React.FC = () => {
     <ProductProvider>
       <div>
         <section className="flex items-center justify-center space-x-10 xl:max-w-5xl">
-          <Pictures/>
-          <section>data</section>
+          <Pictures />
+          <Data />
           <section>price and button</section>
-      
         </section>
         <section>related</section>
       </div>
