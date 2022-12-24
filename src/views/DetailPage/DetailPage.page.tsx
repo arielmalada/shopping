81f35786ed5fac1863afb60b4b78a1f8fbca990c
arielmalada <arielmalada@outlook.com>
2022-12-24 15:09:32 +0200

    feat: checkout section

--- src/views/DetailPage/DetailPage.page.tsx
+++ src/views/DetailPage/DetailPage.page.tsx
@@ -1,6 +1,6 @@
 //#region IMPORTS
 
-import { Typography } from "@material-tailwind/react";
+import { Button, Card, CardBody, Typography } from "@material-tailwind/react";
 import React, { useContext } from "react";
 import {
   ProductContext,
@@ -32,17 +32,38 @@ const Data: React.FC = () => {
       <Typography variant="h2">{productValue?.title}</Typography>
       <div className="flex space-x-2">
         {isPriceReduced && (
-          <div className="text-red-700">
-            <Typography variant="small">Deal</Typography>
-            <Typography>{productValue?.discountPercentage} %</Typography>
+          <div className="text-xs text-red-700 ">
+            <div>Deal</div>
+            <div className="text-sm">{productValue?.discountPercentage} %</div>
           </div>
         )}
-        <div>
-          <Typography>{finalPrice}</Typography>
-          {isPriceReduced && <Typography variant="small">was {productValue?.price}</Typography>}
-        </div>
+        <div>$ {finalPrice}</div>
       </div>
+      {isPriceReduced && (
+        <div className="flex space-x-1 text-xs">
+          <span>was</span>
+          <span className="line-through">$ {productValue?.price}</span>
+        </div>
+      )}
     </div>
+  );
+};
+
+const CheckoutSection: React.FC = () => {
+  const productValue = useContext(ProductContext);
+  const isPriceReduced = (productValue?.discountPercentage || 0) > 0;
+  const finalPrice =
+    isPriceReduced && productValue
+      ? productValue.price -
+        Math.round(productValue.price * productValue.discountPercentage * 0.01)
+      : productValue?.price;
+  return (
+    <Card>
+      <CardBody className="space-y-2">
+        <Typography variant="h4">$ {finalPrice}</Typography>
+        <Button>Add to Cart</Button>
+      </CardBody>
+    </Card>
   );
 };
 //#endregion
@@ -55,7 +76,7 @@ const DetailPage: React.FC = () => {
         <section className="flex items-center justify-center space-x-10 xl:max-w-5xl">
           <Pictures />
           <Data />
-          <section>price and button</section>
+          <CheckoutSection />
         </section>
         <section>related</section>
       </div>
