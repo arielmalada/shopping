4aaa6eb9869ab6a129963cd172d9b0bfeda7a8d8
arielmalada <arielmalada@outlook.com>
2022-12-24 20:47:53 +0200

    feat: shopping cart concept

--- src/views/DetailPage/DetailPage.page.tsx
+++ src/views/DetailPage/DetailPage.page.tsx
@@ -1,5 +1,6 @@
 //#region IMPORTS
 
+import { Button } from "@material-tailwind/react";
 import React, { useState } from "react";
 import CheckoutSection from "../../components/molecules/CheckoutSection";
 import DataSection from "../../components/molecules/DataSection";
@@ -14,18 +15,30 @@ import { ProductTypes } from "../../services/products";
 const DetailPage: React.FC = () => {
   const [cartData, setCartData] = useState<ProductTypes[]>([]);
   const handleClickAddtoCart = (data: ProductTypes) => {
+    // concat the data
     setCartData([...cartData, data]);
   };
+  const handleClickCartButton = () => {
+    // reset the cart
+    setCartData([])
+  }
   return (
     <ProductProvider>
       <ProductsByCategoryProvider>
-        <div className="mx-4 my-10 lg:mx-auto lg:max-w-5xl">
-          <section className="flex flex-col items-center justify-center mb-20 space-y-4 lg:space-x-10 lg:flex-row">
-            <PictureSection />
-            <DataSection />
-            <CheckoutSection onClick={handleClickAddtoCart} />
-          </section>
-          <RelatedSection />
+        <div>
+          <div className="absolute space-x-1 right-2 lg:right-8">
+            <Button className="space-x-1" onClick={handleClickCartButton}>
+              <span>Shopping Cart</span><span>{cartData.length > 0 && cartData.length}</span>
+            </Button>
+          </div>
+          <div className="mx-4 my-10 lg:mx-auto lg:max-w-5xl">
+            <section className="flex flex-col items-center justify-center mb-20 space-y-4 lg:space-x-10 lg:flex-row">
+              <PictureSection />
+              <DataSection />
+              <CheckoutSection onClick={handleClickAddtoCart} />
+            </section>
+            <RelatedSection />
+          </div>
         </div>
       </ProductsByCategoryProvider>
     </ProductProvider>
