430359f2f7a044ea83ca7918e1ba7c9e1c32660f
arielmalada <arielmalada@outlook.com>
2022-12-24 14:03:39 +0200

    feat: base detail page

---
+++ src/views/DetailPage/DetailPage.page.tsx
@@ -0,0 +1,18 @@
+//#region IMPORTS
+
+import React from "react";
+
+//#endregion
+
+//#region MAIN COMPONENT
+const DetailPage: React.FC = () => {
+
+  return (
+    <div>
+      detail page
+    </div>
+  );
+};
+//#endregion
+
+export default DetailPage;
