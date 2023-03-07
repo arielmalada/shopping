9320b1fdf38754136494465252b0c9b3d353e63d
arielmalada <arielmalada@outlook.com>
2023-03-07 22:29:42 +0200

    feat: go back button + fontawesome

--- src/views/DetailPage/DetailPage.page.tsx
+++ src/views/DetailPage/DetailPage.page.tsx
@@ -1,8 +1,10 @@
 //#region IMPORTS
 
-import { Button } from "@material-tailwind/react";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, IconButton } from "@material-tailwind/react";
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import CheckoutSection from "../../components/molecules/CheckoutSection";
 import DataSection from "../../components/molecules/DataSection";
 import PictureSection from "../../components/molecules/PictureSection";
@@ -22,17 +24,25 @@ const DetailPage: React.FC = () => {
   };
   const handleClickCartButton = () => {
     // reset the cart
-    setCartData([])
-  }
-  if(!productID) return null;
+    setCartData([]);
+  };
+  const navigate = useNavigate();
+
+  if (!productID) return null;
   return (
     <ProductProvider productID={productID}>
       <ProductsByCategoryProvider>
         <div>
-          <div className="absolute space-x-1 right-2 lg:right-8">
-            <Button className="space-x-1" onClick={handleClickCartButton}>
-              <span>Shopping Cart</span><span>{cartData.length > 0 && cartData.length}</span>
-            </Button>
+          <div className="flex items-center justify-between px-2 lg:px-8">
+            <IconButton variant="text" onClick={() => navigate(-1)} size="lg">
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </IconButton>
+            <div className="space-x-1">
+              <Button className="space-x-1" onClick={handleClickCartButton}>
+                <span>Shopping Cart</span>
+                <span>{cartData.length > 0 && cartData.length}</span>
+              </Button>
+            </div>
           </div>
           <div className="mx-4 my-10 lg:mx-auto lg:max-w-5xl">
             <section className="flex flex-col items-center justify-center mb-20 space-y-4 lg:space-x-10 lg:flex-row">
